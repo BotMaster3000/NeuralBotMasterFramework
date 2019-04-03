1ac344aa3abc2364f0d3ff3b7e1b923376e24eac
BotMaster3000 <jdgebauer@gmx.de>
2019-04-03 18:52:51 +0200

    Added Interfaces for different Node-Types etc

---
+++ NeuralBotMasterFramework/NeuralBotMasterFramework/Interfaces/IInputLayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralBotMasterFramework.Interfaces
+{
+    public interface IInputLayer
+    {
+        INode[] Nodes { get; set; }
+    }
+}
---
+++ NeuralBotMasterFramework/NeuralBotMasterFramework/Interfaces/INode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralBotMasterFramework.Interfaces
+{
+    public interface INode
+    {
+        decimal Value { get; set; }
+    }
+}
