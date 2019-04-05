5b198d54fa341ff5faf1eeb727d686a5ff9f0ca2
BotMaster3000 <jdgebauer@gmx.de>
2019-04-05 15:01:39 +0200

    Added a new Interface IGeneticAlgorithm

---
+++ NeuralBotMasterFramework/NeuralBotMasterFramework/Interfaces/IGeneticAlgorithm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralBotMasterFramework.Interfaces
+{
+    public interface IGeneticAlgorithm
+    {
+        int TotalNetworks { get; set; }
+        int NetworksToKeep { get; set; }
+        double MutationRate { get; set; }
+        double MutationChance { get; set; }
+
+        Dictionary<IWeightedLayer, int> NetworksAndFitness { get; set; }
+        void SetInput(double[] input);
+        void PropagateAllNetworks();
+        void CalculateFitnesses();
+        void SortByFitness();
+        void BreedBestNetworks();
+    }
+}
