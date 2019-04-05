4310e477df19da25e0d3992f801221a5d38d5539
BotMaster3000 <jdgebauer@gmx.de>
2019-04-05 15:23:42 +0200

    Added GeneticAlgorithm.cs and Added the Constructor with TDD

---
+++ NeuralBotMasterFramework/NeuralBotMasterFramework/Logic/Algorithms/GeneticAlgorithm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeuralBotMasterFramework.Interfaces;
+using NeuralBotMasterFramework.Logic.Networks;
+
+namespace NeuralBotMasterFramework.Logic.Algorithms
+{
+    public class GeneticAlgorithm : IGeneticAlgorithm
+    {
+        public int TotalNetworks { get; set; }
+        public int NetworksToKeep { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double MutationRate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double MutationChance { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Dictionary<IWeightedNetwork, int> NetworksAndFitness { get; set; } = new Dictionary<IWeightedNetwork, int>();
+        public int InputNodes { get; set; }
+        public int HiddenNodes { get; set; }
+        public int HiddenLayers { get; set; }
+        public int OutputNodes { get; set; }
+
+        public GeneticAlgorithm(int totalNetworks, int inputNodes, int hiddenNodes, int hiddenLayers, int outputNodes)
+        {
+            TotalNetworks = totalNetworks;
+            InputNodes = inputNodes;
+            HiddenNodes = hiddenNodes;
+            HiddenLayers = hiddenLayers;
+            OutputNodes = outputNodes;
+
+            InitializeNetworks();
+        }
+
+        private void InitializeNetworks()
+        {
+            for (int i = 0; i < TotalNetworks; ++i)
+            {
+                NetworksAndFitness.Add(new WeightedNetwork(InputNodes, HiddenLayers, HiddenLayers, OutputNodes), 0);
+            }
+        }
+
+        public void BreedBestNetworks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CalculateFitnesses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PropagateAllNetworks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetInput(double[] input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SortByFitness()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NeuralBotMasterFramework/NeuralBotMasterFrameworkTests/Logic/Algorithms/GeneticAlgorithmTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeuralBotMasterFramework.Logic.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralBotMasterFramework.Logic.Algorithms.Tests
+{
+    [TestClass]
+    public class GeneticAlgorithmTests
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            const int TOTAL_NETWORKS = 100;
+            const int INPUT_NODES = 10;
+            const int HIDDEN_NODES = 5;
+            const int HIDDEN_LAYERS = 3;
+            const int OUTPUT_NODES = 3;
+            GeneticAlgorithm algorithm = new GeneticAlgorithm(TOTAL_NETWORKS, INPUT_NODES, HIDDEN_NODES, HIDDEN_LAYERS, OUTPUT_NODES);
+            Assert.AreEqual(TOTAL_NETWORKS, algorithm.TotalNetworks);
+            Assert.AreEqual(TOTAL_NETWORKS, algorithm.NetworksAndFitness.Count);
+        }
+
+        [TestMethod]
+        public void BreedBestNetworksTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void CalculateFitnessesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void PropagateAllNetworksTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void SetInputTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void SortByFitnessTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
