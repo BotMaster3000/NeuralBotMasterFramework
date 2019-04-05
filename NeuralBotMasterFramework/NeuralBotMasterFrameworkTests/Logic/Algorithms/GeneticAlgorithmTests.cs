a0d4e372a08ec962f9f272e7c5f28afcf73d73ff
BotMaster3000 <jdgebauer@gmx.de>
2019-04-05 15:41:19 +0200

    *Appendix to previous commit*

--- NeuralBotMasterFramework/NeuralBotMasterFrameworkTests/Logic/Algorithms/GeneticAlgorithmTests.cs
+++ NeuralBotMasterFramework/NeuralBotMasterFrameworkTests/Logic/Algorithms/GeneticAlgorithmTests.cs
@@ -5,23 +5,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NeuralBotMasterFramework.Helper;
 
 namespace NeuralBotMasterFramework.Logic.Algorithms.Tests
 {
     [TestClass]
     public class GeneticAlgorithmTests
     {
+        private const int TOTAL_NETWORKS = 100;
+        private const int INPUT_NODES = 3;
+        private const int HIDDEN_NODES = 5;
+        private const int HIDDEN_LAYERS = 3;
+        private const int OUTPUT_NODES = 3;
+
         [TestMethod]
         public void ConstructorTest()
         {
-            const int TOTAL_NETWORKS = 100;
-            const int INPUT_NODES = 10;
-            const int HIDDEN_NODES = 5;
-            const int HIDDEN_LAYERS = 3;
-            const int OUTPUT_NODES = 3;
             GeneticAlgorithm algorithm = new GeneticAlgorithm(TOTAL_NETWORKS, INPUT_NODES, HIDDEN_NODES, HIDDEN_LAYERS, OUTPUT_NODES);
             Assert.AreEqual(TOTAL_NETWORKS, algorithm.TotalNetworks);
             Assert.AreEqual(TOTAL_NETWORKS, algorithm.NetworksAndFitness.Count);
+        }
+
+        [TestMethod]
+        public void SetInputTest_ShouldCloneInputData()
+        {
+            double[] input = new double[]
+            {
+                RandomNumberGenerator.GetNextDouble(),
+                RandomNumberGenerator.GetNextDouble(),
+                RandomNumberGenerator.GetNextDouble(),
+            };
+            GeneticAlgorithm algorithm = new GeneticAlgorithm(TOTAL_NETWORKS, INPUT_NODES, HIDDEN_NODES, HIDDEN_LAYERS, OUTPUT_NODES);
+            algorithm.SetInput(input);
+            for(int i = 0; i < input.Length; ++i)
+            {
+                Assert.AreEqual(input[i], algorithm.CurrentInput[i]);
+            }
+
+            input = new double[]
+            {
+                RandomNumberGenerator.GetNextDouble(),
+            };
+            Assert.AreNotEqual(input.Length, algorithm.CurrentInput.Length);
         }
 
         [TestMethod]
@@ -38,12 +63,6 @@ namespace NeuralBotMasterFramework.Logic.Algorithms.Tests
 
         [TestMethod]
         public void PropagateAllNetworksTest()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod]
-        public void SetInputTest()
         {
             Assert.Fail();
         }
