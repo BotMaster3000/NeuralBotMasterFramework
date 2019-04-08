5040cd21579cfbf84587cb54ddd4f0b0f8d29fc6
BotMaster3000 <jdgebauer@gmx.de>
2019-04-08 18:29:02 +0200

    Added IBreedingPoolGenerator and implementation IndexBasedPoolGenerator

---
+++ NeuralBotMasterFramework/NeuralBotMasterFramework/Logic/PoolGenerators/IndexBasedPoolGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeuralBotMasterFramework.Interfaces;
+
+namespace NeuralBotMasterFramework.Logic.PoolGenerators
+{
+    public class IndexBasedPoolGenerator : IBreedingPoolGenerator
+    {
+        public List<IWeightedNetwork> GenerateBreedingPool(Dictionary<IWeightedNetwork, double> networksAndFitness)
+        {
+            List<IWeightedNetwork> poolList = new List<IWeightedNetwork>();
+            for (int networkIndex = 0; networkIndex < networksAndFitness.Count; ++networkIndex)
+            {
+                IWeightedNetwork currentNetwork = networksAndFitness.Keys.ElementAt(networkIndex);
+                for (int i = 0; i < networksAndFitness.Count - networkIndex; ++i)
+                {
+                    poolList.Add(currentNetwork);
+                }
+            }
+            return poolList;
+        }
+    }
+}
